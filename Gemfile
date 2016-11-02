be5e183640c3969215930272a2ae8c0f76d30acf
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2016-11-02 08:00:02 +0000

    includes rack attack

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem "figaro"
 gem 'active_model_serializers'
 gem 'rack-cors'
+gem 'rack-attack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module OpenGovernmentMontanaApi
       end
     end
 
+    config.middleware.use Rack::Attack
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
